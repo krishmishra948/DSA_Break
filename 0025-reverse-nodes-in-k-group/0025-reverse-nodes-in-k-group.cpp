010c206a9c5f09886b2e0e98a3752cfb174a81af
krishmishra948 <krishmishram@gmail.com>
2026-08-20 17:57:19 +0530

    Time: 0 ms (100%), Space: 16.3 MB (96.97%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -1,77 +1,28 @@
 class Solution {
 public:
+  ListNode*reverseKGroup(ListNode*head,int k){
+    ListNode*temp=head;
+    int cnt=0;
+    //check if k nodes exist
+    while(cnt< k){
+        if(temp==NULL) return head;
+        temp=temp->next;
+        cnt++;
 
-    ListNode* getKthNode(ListNode* temp, int k) {
-
-        k = k - 1;
-
-        while (temp != NULL && k > 0) {
-            k--;
-            temp = temp->next;
-        }
-
-        return temp;
     }
-
-
-    ListNode* reverseLinkedList(ListNode* head) {
-
-        ListNode* prev = NULL;
-        ListNode* temp = head;
-
-        while (temp != NULL) {
-
-            ListNode* nextNode = temp->next;
-
-            temp->next = prev;
-
-            prev = temp;
-            temp = nextNode;
-        }
-
-        return prev;
-    }
-
-
-    ListNode* reverseKGroup(ListNode* head, int k) {
-
-        ListNode* temp = head;
-        ListNode* prevLast = NULL;
-
-        while (temp != NULL) {
-
-            ListNode* kThNode = getKthNode(temp, k);
-
-
-            if (kThNode == NULL) {
-
-                if (prevLast != NULL) {
-                    prevLast->next = temp;
-                }
-
-                break;
+        //recursively call for the rest of the LL
+           ListNode* prevNode = reverseKGroup(temp, k);
+            // reverse current group
+            temp=head;
+            cnt=0;
+            while(cnt< k){
+                ListNode*next=temp->next;
+                temp->next=prevNode;
+                prevNode=temp;
+                temp=next;
+                cnt++;
             }
-
+                return prevNode;
+  }
     
-            ListNode* nextNode = kThNode->next;
-            kThNode->next = NULL;
-
-            reverseLinkedList(temp);
-
-
-            if (temp == head) {
-                head = kThNode;
-            }
-            else {
-                prevLast->next = kThNode;
-            }
-
-            prevLast = temp;
-
-            
-            temp = nextNode;
-        }
-
-        return head;
-    }
 };
