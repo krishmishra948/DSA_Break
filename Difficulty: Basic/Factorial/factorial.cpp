c8b49cde3e66962092e3084527cc5277a8e537b0
krishmishra948 <krishmishram@gmail.com>
2026-09-17 15:37:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Factorial/factorial.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    int factorial(int n) {
+        // code here
+        if(n==0 || n==1) return 1;
+        else{
+            return n*factorial(n-1);
+    }
+}
+    
+};
