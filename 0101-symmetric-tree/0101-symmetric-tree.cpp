d22c3bcbf1940122acdb5ee4193b1112c6bd7aeb
krishmishra948 <krishmishram@gmail.com>
2026-09-26 12:44:20 +0530

    Time: 0 ms (100%), Space: 18.5 MB (42%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+
+    bool check(TreeNode* left, TreeNode* right) {
+
+        if(left == NULL || right == NULL)
+            return left == right;
+
+        if(left->val != right->val)
+            return false;
+
+        return check(left->left, right->right) &&
+               check(left->right, right->left);
+    }
+
+    bool isSymmetric(TreeNode* root) {
+
+        if(root == NULL)
+            return true;
+
+        return check(root->left, root->right);
+    }
+};
